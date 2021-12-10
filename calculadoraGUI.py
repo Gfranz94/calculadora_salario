19b59e803f646dd08de086f132879573e76e6993
Gustavo B Franz <95686750+Gfranz94@users.noreply.github.com>
2021-12-10 19:11:12 -0300

    Update calculadoraGUI.py

--- calculadoraGUI.py
+++ calculadoraGUI.py
@@ -1,10 +1,5 @@
 import PySimpleGUI as sg
 
-''' implementar quinquênio (5% cada) entra para previdencia e irrf
-    implementar GDPI (é livre de qualquer desconto previdenciário e pecuniário
-    trocar a ordem de algumas perguntas, quem sabe separar um prog para temporarios
-    e outro para efetivos?'''
-
 
 #Esta função calcula o IRRF
 def leao(base_irrf, num_dependente):
@@ -31,6 +26,8 @@ def leao(base_irrf, num_dependente):
     return imposto
 #esta função calcula o desconto do INSS para os profs temporarios
 def INSS(salario_bruto):
+    '''esta função calcula o desconto do INSS para os profs temporarios levando em
+    consideração o deconto do INSS no vale alimentação'''
 
     inss = 0
 
@@ -46,55 +43,7 @@ def INSS(salario_bruto):
         inss = 751.99
     return inss
 
-def salario_temporario(num_dependente, substituição, vencimento_base, jornada_base, alimentacao):
-
-    aprimoramento = 500
-
-
-    imposto = previdencia = salario_bruto = salario_liquido = 0
-
-    if substituição == 0:
-
-        salario_bruto = vencimento_base + alimentacao
-
-    else:
-
-        salario_bruto = vencimento_base + vencimento_base*(substituição/jornada_base) + alimentacao
-
-    previdencia = INSS(salario_bruto)
-
-    base_irrf = salario_bruto - previdencia - alimentacao
-
-    imposto = leao(base_irrf, num_dependente)
-
-    salario_liquido = salario_bruto - previdencia - imposto + aprimoramento
-
-    return salario_liquido
-
-def salario_efetivo(num_dependente, substituição, letra, vencimento_base, gratificacao_mestrado, jornada_base, alimentacao):
-
-    aprimoramento = 500
-
-    imposto = previdencia = salario_bruto = salario_liquido = 0
-
-    if substituição == 0:
-
-        salario_bruto = vencimento_base
-
-    else:
-
-        salario_bruto = vencimento_base + vencimento_base*(substituição/jornada_base) + gratificacao_mestrado
-
-    previdencia = (vencimento_base + gratificacao_mestrado)*0.1425
-
-    base_irrf = salario_bruto - previdencia
-
-    imposto = leao(base_irrf, num_dependente)
-
-    salario_liquido = salario_bruto - previdencia - imposto + aprimoramento + alimentacao
-
-    return salario_liquido
-
+#Converte a letra em número para calculo do salario do prof efetivo
 def conversor_letra(h_letra):
     if h_letra == 'A':
         letra = 0
@@ -111,13 +60,15 @@ def conversor_letra(h_letra):
     if h_letra == 'G':
         letra = 6
     return letra
-#layout
+
+
+#layout do programa
 layout = [
     [sg.Text('Bem-vindo à calculadora de salário dos professores da SEDUC GO!')],
     [sg.Text('Esta versão do programa *NÃO* inclui no cálculo de salário:')],
     [sg.Text('===> Quinquênio')],
     [sg.Text('===> Bônus por Resultado ou Bônus Fundeb')],
-    [sg.Text('===> GDPI')],
+    [sg.Text('Sua escola é de período integral?'), sg.Radio('Sim', 'GDPI', key='integral'), sg.Radio('Não', 'GDPI')],
     [sg.Text('Carga horária base: '), sg.Combo(['20 horas', '30 horas', '40 horas'], key='carga_horaria', size=(8, 1))],
     [sg.Text('Nº de aulas de complementação da carga horária: '), sg.Spin([i for i in range(0, 15)], initial_value=0, key='complementacao', size=(2, 1))],
     [sg.Text('Tem auxílio internet de 100 reais? '), sg.Radio('Sim', 'internet', key='tem_100'), sg.Radio('Não', 'internet', key='n_tem_100')],
@@ -128,16 +79,22 @@ layout = [
     [sg.Text('Você é: '), sg.Radio('P3', 'posicao_vertical', key='eh_p3'), sg.Radio('P4', 'posicao_vertical', key='eh_p4')],
     [sg.Text('Sua letra é: '), sg.Combo(['A', 'B', 'C', 'D', 'E', 'F', 'G'], key='h_letra')],
     [sg.Text('Você possui mestrado? '), sg.Radio('Sim', 'p_mestrado', key='tem_mestrado'), sg.Radio('Não', 'p_mestrado', key='n_tem_mestrado')],
+    [sg.Text('Você possui quantos quinquênios(adicional tempo de serviço)?'), sg.Spin([i for i in range(0,21)], initial_value=0, key='quinquenio', size=(2, 1))],
     [sg.Output(size=(63, 10))],
     [sg.Button('Calcular!'), sg.Button('Sair')]
 ]
-#janela
+
+
+#janela do programa
 window = sg.Window('Calculadora de Salário GUI version 1.0 by Gustavo B. Franz', layout, icon=r'D:\Calculadora Salário\Versão GUI\Flag_of_Goias.ico')
 
+#loop mantém a janela aberta após execução do programa
 while True:
     evento, valores = window.read()
-    if evento is None or evento == 'Sair':
-        break
+    if evento is None or evento == 'Sair':  
+        break #fecha o programa
+
+    #valores dos testes lógicos e de algumas variáveis informados pelo usuário
     carga_horaria = valores['carga_horaria']
     complementacao = valores['complementacao']
     tem_internet = valores['tem_100']
@@ -145,27 +102,42 @@ while True:
     eh_efetivo = valores['eh_efetivo']
     eh_p3 = valores['eh_p3']
     eh_p4 = valores['eh_p4']
+    integral = valores['integral']
     h_letra = valores['h_letra']
     tem_mestrado = valores['tem_mestrado']
     é_sindicalizado = valores['eh_sind']
-
+    quant_quinquenio = valores['quinquenio']
 
     #variaveis globais
-    vencimento_base = sintego = internet = alimentacao = ch_base = 0
-
-    #coleta o valor da carga horária básica
+    vencimento_base = sintego = internet = alimentacao = gdpi = 0
+    aprimoramento = 500
+    
+    #aplica o valor da carga horaria base e o respectivo vale alimentacao
     if carga_horaria == '20 horas':
+
         ch_base = 20
+
+        alimentacao = 250
+
     if carga_horaria == '30 horas':
+
         ch_base = 30
+
+        alimentacao = 500
+
     if carga_horaria == '40 horas':
+
         ch_base = 40
+
+        alimentacao = 500
 
     #calcula o número de aulas correspondente à carga horaria informada e converte para o código correspondente da CRE
     jornada_base = round((ch_base*0.7)*7.51)
 
-
     substituição = round(complementacao*7.51)
+
+    if integral:
+        gdpi = 2000
 
     if tem_internet:
         internet = 100
@@ -175,28 +147,20 @@ while True:
         #listas abaixo contém o valor do vencimento base de acordo com a carga horaria (linhas) e letra (colunas)
         p3 = [[1789.84, 1825.63, 1862.15, 1899.40, 1937.38, 1976.12, 2015.64], [2684.74, 2738.45, 2793.23, 2849.09, 2906.07, 2964.19, 3023.47], [3579.68, 3651.25, 3724.29, 3798.78, 3874.76, 3952.25, 4031.29]]
         p4 = [[2018.05, 2058.41, 2099.57, 2141.56, 2184.40, 2228.08, 2272.64], [3027.07, 3087.61, 3149.35, 3212.34, 3276.58, 3342.11, 3408.95], [4036.09, 4116.81, 4199.13, 4283.12, 4368.78, 4456.15, 4545.28]]
-        #coleta info acerca da posição vertical do prof na carreira
-
-
+   
         if eh_p3:
 
             if carga_horaria == '20 horas':
 
                 vencimento_base = p3[0]
 
-                alimentacao = 250
-
             if carga_horaria == '30 horas':
 
                 vencimento_base = p3[1]
 
-                alimentacao = 500
-
             if carga_horaria == '40 horas':
 
                 vencimento_base = p3[2]
-
-                alimentacao = 500
 
         if eh_p4:
 
@@ -204,26 +168,19 @@ while True:
 
                 vencimento_base = p4[0]
 
-                alimentacao = 250
-
             if carga_horaria == '30 horas':
 
                 vencimento_base = p4[1]
 
-                alimentacao = 500
-
             if carga_horaria == '40 horas':
-
-                alimentacao = 500
 
                 vencimento_base = p4[2]
 
-        #coleta info acerca do posição horizontal do prof na carreira
+        #austa info acerca da posição horizontal do prof na carreira
         letra = conversor_letra(h_letra)
 
         vencimento_base = vencimento_base[letra]
 
-        #coleta info sobre mestrado do prof
         gratificacao_mestrado = 0
 
         if tem_mestrado:
@@ -234,10 +191,27 @@ while True:
 
             sintego = vencimento_base*0.01
 
+        vencimento_base = vencimento_base + (vencimento_base*0.05)*quant_quinquenio
 
-        #calcula o salario liquido do efetivo
-        salario_liquido = salario_efetivo(num_dependente, substituição, letra, vencimento_base, gratificacao_mestrado, jornada_base, alimentacao) - sintego + internet
+        if substituição == 0:
 
+            salario_bruto = vencimento_base + gratificacao_mestrado
+
+        else:
+
+            salario_bruto = vencimento_base + vencimento_base*(substituição/jornada_base) + gratificacao_mestrado
+
+        previdencia = (vencimento_base + gratificacao_mestrado)*0.1425
+
+        base_irrf = salario_bruto - previdencia + gdpi
+
+        imposto = leao(base_irrf, num_dependente)
+
+        salario_liquido = salario_bruto - previdencia - imposto + gdpi - sintego + internet + alimentacao + aprimoramento
+
+        proventos = salario_bruto + internet + alimentacao + aprimoramento + gdpi
+
+        print('\nSeus proventos são de R$', proventos)
         print('\nO seu salário líquido é de R$', salario_liquido)
         print('\nPapai Caiado não está pagando mal hein ( ͡° ͜ʖ ͡°)')
 
@@ -250,26 +224,36 @@ while True:
 
             vencimento_base = ptemp[0]
 
-            alimentacao = 250
-
         if carga_horaria == '30 horas':
 
             vencimento_base = ptemp[1]
-
-            alimentacao = 500
 
         if carga_horaria == '40 horas':
 
             vencimento_base = ptemp[2]
 
-            alimentacao = 500
-
         if é_sindicalizado:
 
             sintego = vencimento_base*0.01
 
-        #calcula o salario liquido do temporario
-        salario_liquido = salario_temporario(num_dependente, substituição, vencimento_base, jornada_base, alimentacao) - sintego + internet
+        if substituição == 0:
 
+            salario_bruto = vencimento_base + alimentacao
+
+        else:
+
+            salario_bruto = vencimento_base + vencimento_base*(substituição/jornada_base) + alimentacao
+
+        previdencia = INSS(salario_bruto)
+
+        base_irrf = salario_bruto - previdencia - alimentacao
+
+        imposto = leao(base_irrf, num_dependente)
+
+        salario_liquido = salario_bruto - previdencia - imposto - sintego + internet + aprimoramento
+
+        proventos = salario_bruto + internet + aprimoramento
+
+        print('\nSeus proventos são de R$', proventos)
         print('\nO seu salário líquido é de R$', salario_liquido)
         print('\nPapai Caiado não está pagando mal hein ( ͡° ͜ʖ ͡°)')
